ea2f1162b5fe2c933e45c7c39e7b865291474114
lucix17 <91908402+mihirx17@users.noreply.github.com>
2022-09-23 14:47:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public  static  void inverted_star_pattern() {
+        int row, cl;
+        System.out.println("Enter your Row values===");
+        Scanner Sc = new Scanner(System.in);
+        row = Sc.nextInt();
+        for (int i = row; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+    public static void main(String[] args) {
+inverted_star_pattern();
+
+    }
+}
